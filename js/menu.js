966e3b476b2d8f3c727bba1937fcbc62ca7bf27d
Carolina0204 <mejiascaro23@gmail.com>
2020-06-20 00:33:57 -0500

    final

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,15 @@
 const menut = document.querySelector('.menu'); //selecciona la primera clase que se nombre 'menu'
 const menuNav = document.querySelector('.menu-navegador');
 
-console.log(menut)
-console.log(menuNav)
+//console.log(menut)
+//console.log(menuNav)
 
 menut.addEventListener('click', () => {
     menuNav.classList.toggle('spread')
+})
+
+window.addEventListener('click', e => {
+    if (menuNav.classList.contains('spread') && e.target != menuNav && e.target != menut) {
+        menuNav.classList.toggle('spread')
+    }
 })
